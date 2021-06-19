be66359dcb884a18ba3ac79d372ff2059d1542a1
bloges56 <brady.drew.logan@gmail.com>
2021-06-18 19:11:12 -0500

    can now encrypt messages with a rail fence cipher

--- Program.cs
+++ Program.cs
@@ -6,7 +6,60 @@ namespace RailFenceCipher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string plainText = System.IO.File.ReadAllText(args[0]).ToLower();
+
+            string cipherText = Encrypt(plainText, plainText, Int32.Parse(args[1]), 1, 1, true);
+
+            Console.WriteLine(cipherText);
+        }
+
+        public static string Encrypt(string plainText, string modifiedText, int depth, int curLevel, int lowestLevel, bool rising)
+        {
+            if(lowestLevel > depth)
+            {
+                return "";
+            }
+            if(modifiedText == "")
+            {
+                return "" + Encrypt(plainText, plainText, depth, 1, ++lowestLevel, true);
+            }
+            if (curLevel == lowestLevel)
+            {
+                if(curLevel == depth)
+                {
+                    return modifiedText[0] + Encrypt(plainText, modifiedText.Substring(1), depth, --curLevel, lowestLevel, false);
+                }
+                if(curLevel == 1)
+                {
+                    return modifiedText[0] + Encrypt(plainText, modifiedText.Substring(1), depth, ++curLevel, lowestLevel, true);
+                }
+
+                if(rising)
+                {
+                    return modifiedText[0] + Encrypt(plainText, modifiedText.Substring(1), depth, ++curLevel, lowestLevel, true);
+                }
+
+                return modifiedText[0] + Encrypt(plainText, modifiedText.Substring(1), depth, --curLevel, lowestLevel, false);
+
+            }
+
+            if(curLevel == depth)
+            {
+                return "" + Encrypt(plainText, modifiedText.Substring(1), depth, --curLevel, lowestLevel, false);
+            }
+
+            if(curLevel == 1)
+            {
+                return "" + Encrypt(plainText, modifiedText.Substring(1), depth, ++curLevel, lowestLevel, true);
+            }
+
+            if(rising)
+            {
+                return "" + Encrypt(plainText, modifiedText.Substring(1), depth, ++curLevel, lowestLevel, true);
+            }
+
+            return "" + Encrypt(plainText, modifiedText.Substring(1), depth, --curLevel, lowestLevel, false);
+
         }
     }
 }
